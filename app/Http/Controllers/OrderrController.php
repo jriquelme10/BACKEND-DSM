14602e74eb2326351e82ad5b4da525fc0b80800d
Víctor M <victormunozfer@gmail.com>
2022-12-11 13:17:41 -0300

    Update orderrController

--- app/Http/Controllers/OrderrController.php
+++ app/Http/Controllers/OrderrController.php
@@ -22,49 +22,9 @@ class OrderrController extends Controller
         ]);
     }
 
-    public function detallesOrden(int $order_id)
+    public function detailsOrder(int $order_id)
     {
         $orderrs = DB::table('orders_products')->where('order_id', '=', $order_id)->get();
         return response()->json($orderrs);
-    }
-
-
-    /*public function saveOrder(Request $request)
-    {
-        $arr = [
-            'order' => [
-
-                'totalAmount' => $request->totalAmount,
-                'number_table' => $request->number_table,
-                'status' => $request->status
-
-            ],
-            'resumeOrders' => $request->orders_products
-
-        ];
-
-        $order = Orderr::create($arr['order']);
-        $detailsOrden = $request->orders_products;
-        foreach ($detailsOrden as $detail) {
-            $product = Producto::where('id', $detail['product_id'],)->FirstOrFail();
-            $product->save();
-            OrderProduct::create([
-                'order_id' => $order->id,
-                'product_id' => $detail['product_id'],
-                'quantity' => $detail['quantity']
-            ]);
-        }
-
-        return OrderProduct::get();
-    }
-    public function listar_pedidos()
-    {
-        $pedidos = Orderr::all();
-
-        return view("administrarOrdenes")->with('reumen_ordens', $pedidos);
-    }
-    */
-    public function atender_orden()
-    {
     }
 }
